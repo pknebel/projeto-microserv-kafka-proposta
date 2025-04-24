81bb914be45d89581c0d0deaf7efb82cdeddc39b
Pedro Henrique Knebel <pedro.knebel@expressosaomiguel.com.br>
2025-04-24 09:48:22 -0300

    finalizando cotação

--- src/main/java/com/pedro/controllers/ProposalController.java
+++ src/main/java/com/pedro/controllers/ProposalController.java
@@ -3,9 +3,10 @@ package com.pedro.controllers;
 import com.pedro.dto.ProposalDTO;
 import com.pedro.dto.ProposalDetailsDTO;
 import com.pedro.services.ProposalService;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
+import jakarta.annotation.security.RolesAllowed;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,9 +14,11 @@ import org.slf4j.LoggerFactory;
 public class ProposalController {
 
     private final Logger LOG = LoggerFactory.getLogger(ProposalController.class);
-    private final ProposalService proposalService;
 
-    @jakarta.inject.Inject
+    @Inject
+    ProposalService proposalService;
+
+    @Inject
     public ProposalController(ProposalService proposalService) {
         this.proposalService = proposalService;
     }
@@ -24,5 +27,34 @@ public class ProposalController {
     @Path("/{id}")
     public ProposalDetailsDTO findDetailsProposal(@PathParam("id") Long id) {
         return proposalService.findFullProposal(id);
+    }
+
+    @POST
+    public Response createProposal(ProposalDetailsDTO proposalDetails) {
+
+        LOG.info("--- Recebendo Proposta de Compra ---");
+
+        try{
+
+            proposalService.createNewProposal(proposalDetails);
+
+            return Response.ok().build();
+
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response removeProposal(@PathParam("id") Long id) {
+        try {
+            proposalService.removeProposal(id);
+            return Response.ok().build();
+            
+        } catch (Exception e) {
+            return Response.serverError().build();
+
+        }
     }
 }
